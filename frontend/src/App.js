023c5775b4595fcdf2629135957072da5492bcf4
Jenushan <jenushan349@gmail.com>
2025-06-24 23:02:47 -0400

    Add upload image to home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [selectImage, setSelectImage] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.file[0];
+    setSelectImage(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Upload Image: </p>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
     </div>
   );
 }
